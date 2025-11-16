3355972d79c1dc2ea6ccd7642a8c296658335b32
Paulo Andre <p.souz436@gmail.com>
2025-11-16 14:21:24 -0300

    Atualizando modelo de usuario

--- src/models/User.js
+++ src/models/User.js
@@ -1,44 +1,28 @@
-
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
-  nome: {
-    type: String,
-    required: true,
-    trim: true
+const UserSchema = new mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    senha: {
+      type: String,
+      required: true
+    }
   },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-
-  senha: {
-    type: String,
-    required: true,
-    minlength: 6
+  {
+    timestamps: true
   }
-}, { timestamps: true });
+);
 
+module.exports = mongoose.model('User', UserSchema);
 
-//  Antes de salvar, criptografa a senha caso ela tenha sido alterada
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('senha')) return next();
-
-  const salt = await bcrypt.genSalt(10);
-  this.senha = await bcrypt.hash(this.senha, salt);
-  next();
-});
-
-
-// Método de comparar senhas ao fazer login
-userSchema.methods.compararSenha = async function (senhaDigitada) {
-  return await bcrypt.compare(senhaDigitada, this.senha);
-};
-
-
-module.exports = mongoose.model('User', userSchema);
